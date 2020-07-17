449239ffa2ed2551c3d46657cc467773803e9807
Karan <karansingh1559@gmail.com>
2020-07-17 20:14:40 +0530

    added basic

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    );
+  }
+}
+
+export default App;
